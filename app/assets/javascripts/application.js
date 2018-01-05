d70a94186719b93191171f9da6826261056ec7e3
ced <zuger.cedric@gmail.com>
2018-01-05 09:31:06 +0100

    D3 integrated.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,5 +11,6 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require turbolinks
+//= require d3
+
 //= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
-  get 'task_records/show'
+  get 'tasks_records/show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'task_records#show'
+  root 'tasks_records#show'
 
 end
