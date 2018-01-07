e45842b3a1cda3f2fdf5a331c5206c6bae37fe2d
ced <zuger.cedric@gmail.com>
2018-01-07 09:45:51 +0100

    Removing server specific informations in get_data.sh

--- script/get_data.sh
+++ script/get_data.sh
@@ -1,8 +1,6 @@
 export PATH="$HOME/.rbenv/bin:$PATH"
 eval "$(rbenv init -)"
 
-cd /var/www/trello-coach/current
+cd $1
 
 RAILS_ENV=production bundle exec rake data:retrieve 2>>log/daily_rake.err >>log/daily_rake.log
-
-# Test
