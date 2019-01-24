1ffafce287b0348a54c3d2500d4d93d3847799ea
ced <zuger.cedric@gmail.com>
2019-01-24 11:24:08 +0100

    Deployment link mail_user_data

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :rbenv_ruby, '2.4.2'
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, 'config/database.yml', 'config/secrets.yml', 'db/production.sqlite3'
+append :linked_files, 'config/mail_user_data.yml', 'config/database.yml', 'config/secrets.yml', 'db/production.sqlite3'
 
 # Default value for linked_dirs is []
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system', 'credentials'
